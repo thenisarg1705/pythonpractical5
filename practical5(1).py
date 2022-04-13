ca483a93b44b3a81233c582e4200809170696e83
thenisarg1705 <97789612+thenisarg1705@users.noreply.github.com>
2022-04-13 18:57:52 +0530

    Create practical5(1).py

---
+++ practical5(1).py
@@ -0,0 +1,17 @@
+# sWAP cASE in Python 
+def swap_case(s):
+    #START
+    Output = '';
+    for char in s:
+        if(char.isupper()==True):
+            Output += (char.lower());
+        elif(char.islower()==True):
+            Output += (char.upper());
+        else:
+            Output += char;
+    return Output;
+    #END
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
